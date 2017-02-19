cc8a6b68ec1c71e4dddca62a8cd27cef16bd71a6
Guilherme Graça <Guilherme Graa>
2017-02-19 00:59:42 +0000

    Added streets

---
+++ Assets/Games/Runner/MyStreets/GenerateBuildings.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateBuildings : MonoBehaviour {
+
+    public Transform[] prefabs;
+
+	void Start () {
+        Transform building;
+
+        building = InstantiateBuilding();
+        building.position += new Vector3(11, 0, -10);
+        building.eulerAngles = new Vector3(0, 90, 0);
+
+        building = InstantiateBuilding();
+        building.position += new Vector3(11, 0, 5);
+        building.eulerAngles = new Vector3(0, 90, 0);
+
+        building = InstantiateBuilding();
+        building.position += new Vector3(-11, 0, -10);
+        building.eulerAngles = new Vector3(0, -90, 0);
+
+        building = InstantiateBuilding();
+        building.position += new Vector3(-11, 0, 5);
+        building.eulerAngles = new Vector3(0, -90, 0);
+
+    }
+	
+	Transform InstantiateBuilding()
+    {
+        Transform prefab = prefabs[UnityEngine.Random.Range(0, prefabs.Length - 1)];
+        Transform building = Instantiate(prefab, transform.position, Quaternion.identity);
+        building.transform.parent = gameObject.transform;
+        return building;
+    }
+}
