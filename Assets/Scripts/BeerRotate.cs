303fa094dacf9ad1b650b46129c0fdd90d25a476
Pedro Miguel Domingos da Costa <pmdcosta@outlook.com>
2017-02-19 02:03:25 +0000

    Merged with ggraca

---
+++ Assets/Scripts/BeerRotate.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeerRotate : MonoBehaviour {
+
+	public int x = 0;
+	public int y = 90;
+	public int z = 15;
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (new Vector3 (x, y, z) * Time.deltaTime);
+	}
+}
