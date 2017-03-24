68232367ed97b300dd6294f1c9e70c134155d1dd
Diogo Bastos <diogodebastos18@gmail.com>
2017-03-24 18:27:28 +0000

    Count rotations with pin interruption

--- Arduino/accel_vr/accel_vr.ino
+++ Arduino/accel_vr/accel_vr.ino
@@ -1,4 +1,4 @@
-#define pinA         A0
+#define pinA A0
 
 boolean state = false;
 boolean lastState = false;
@@ -7,46 +7,46 @@ float velocity;
 
 float accel;
 float accelArray[] = {0,0};
-int wait = 250;
+int step = 300;
 
 int turnsCounter = 0;
+
+/*
+ * Use AnalogPin as interrupt
+ */
+
+void pciSetup(byte pin)
+{
+    *digitalPinToPCMSK(pin) |= bit (digitalPinToPCMSKbit(pin));  // enable pin
+    PCIFR  |= bit (digitalPinToPCICRbit(pin)); // clear any outstanding interrupt
+    PCICR  |= bit (digitalPinToPCICRbit(pin)); // enable interrupt for the group
+}
+
+ISR (PCINT1_vect) // handle pin change interrupt for A0 to A5 here
+{
+  turnsCounter++;
+}  
 
 void setup() { 
   pinMode(pinA, INPUT);
   Serial.begin(9600);
+  pciSetup(pinA);
 }
 
-void loop() {
-  countTurns(analogRead(pinA));
-      
-  if (millis() % wait == 0) {
+void loop() {      
+  if (millis() % step == 0) {
     getAccel();
   }
 }
 
-int countTurns(int val){
-  
-  if (val > 1000) {
-    state = true;
-  } else {
-    state = false;
-  }
-
-  if (state != lastState){
-    turnsCounter++;
-  }
-  
-  lastState = state;
-  return turnsCounter;
-}
-
 void getAccel(){
-  velocity = ((turnsCounter * 0.2/2) / (wait/1000.0)) ;
+  //Serial.println("--");
+  velocity = ((turnsCounter * 0.2/2) / (step/1000.0)) ;
   accelArray[1] = velocity;
-  accel = (accelArray[1]-accelArray[0]) / (wait/1000.0);
+  accel = (accelArray[1]-accelArray[0]) / (step/1000.0);
   accelArray[0] = accelArray[1];
   Serial.println(accel);
   turnsCounter = 0;
-  delay(10);
+  delay(5);
 }
 
